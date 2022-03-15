66572e90bfd3cc626d924c1baae7ac7de8428368
Laili <amalialaili31@gmail.com>
2022-03-15 17:43:12 +0700

    push introduction react

---
+++ 10_Introduction React/praktikum/my-app/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './component/Home';
+import './asset/css/main.css';
+import './asset/css/bootstrap.min.css';
+import Contact_Us from "./component/Contact_Us";
+import About from "./component/About"
+
+
+
+function App() {
+  return (
+    <>
+    {/* <Home/> */}
+    <Contact_Us/>
+    {/* <About/> */}
+    </>
+
+  );
+}
+
+export default App;
---
+++ 10_Introduction React/summary.md
@@ -0,0 +1,8 @@
+* React merupakan library (yang mengatur adalah code kita), bukan framework (code kota diatur oleh framework)
+* Kenapa React
+1. Deklaratif
+2. Berbasis Komponen
+3. Belajar sekali, tulis dimana saja
+4. Sudah Teruji
+5. Teknologi yang Popular
+* Virtual DOM adalah representasi dari UI berbentuk javascript Object yang disimpan pada memori.
